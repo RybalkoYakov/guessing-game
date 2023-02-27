ae786b5b9056154c1ebc697d368103ad9c0f2893
RybalkoYakov <RybalkoYakovv@gmail.com>
2023-02-27 11:46:14 +0300

    19 pass

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    range = []
+
+    constructor() {
+
+    }
 
     setRange(min, max) {
-
+        for (let i = min; i <= max; i++) {
+            this.range.push(i);
+        }
     }
 
     guess() {
-
+        return this.range[Math.floor(this.range.length/2)];
     }
 
     lower() {
-
+        this.range = this.range.slice(0 , this.range.indexOf(this.guess()))
     }
 
     greater() {
-
+        this.range = this.range.slice(this.range.indexOf(this.guess()) + 1, this.range.length)
     }
 }
 
